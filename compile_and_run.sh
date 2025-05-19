c68481f56d0fca5ef6b9f7dc85f2f23252a45d58
Artur Majewski <artur.majewski@gecko-lab.com>
2025-05-19 15:26:00 +0200

    Trivial build system and fixed formatting for zsh

---
+++ compile_and_run.sh
@@ -0,0 +1,6 @@
+# !/usr/bin/zsh
+
+bat hello_world.c
+gcc hello_world.c
+
+./a.out
--- hello_world.c
+++ hello_world.c
@@ -3,6 +3,8 @@
 int main(){
 
 	printf("Hello World\n");
+	int value = 4;
+	printf("2+2 = %d\n",value);
 
 	return 0;
 }
